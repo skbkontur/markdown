c6f58a9771818163b7f9ff45f9dc5c9b0c099194
tsypilov <tsypilov_m@skbkontur.ru>
2023-07-12 18:18:39 +0500

    Add helpers exports

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
 export { Markdown, MarkdownProps } from './src/Markdown/Markdown';
+export { MarkdownCombination } from './src/Markdown/MarkdownHelpers/MarkdownCombination';
+export {
+  markdownHelpItems,
+  markdownHelpFiles,
+  markdownHelpLists,
+  markdownHelpOther,
+} from './src/Markdown/MarkdownHelpItems';
 export { MarkdownViewer } from './src/MarkdownViewer/MarkdownViewer';
 export { MarkdownTheme } from './src/styles/theme';
 export { MarkdownApi, RefItem, User, Token, HorizontalPaddings, ViewMode } from '././src/Markdown/types';
