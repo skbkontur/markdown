98f03295e30c04dfb375c96b329f26edaa475f6c
Azad MAMEDOV <mamedov.a@skbkontur.ru>
2024-12-13 19:14:56 +0400

    Рефакторинг стилей

--- src/Markdown/Emoji/Emoji.styled.ts
+++ src/Markdown/Emoji/Emoji.styled.ts
@@ -1,33 +1,13 @@
 import styled from '../../styles/styled-components';
 
-interface EmojiPickerWrapperProps {
-  backgroundRgb?: string;
-  borderRadius?: number;
-  categoryIconSize?: string;
-  colorBorder?: string;
-  colorBorderOver?: string;
-  fontFamily?: string;
-  fontSize?: string;
-  rgbAccent?: string;
-  rgbBackground?: string;
-  rgbColor?: string;
-  rgbInput?: string;
-  shadow?: string;
-}
+/**
+ * Все доступные переменные для кастомизации стилей можно посмотреть здесь:
+ * https://github.com/missive/emoji-mart/blob/main/packages/emoji-mart-website/example-custom-styles.html
+ */
 
-export const EmojiPickerWrapper = styled.div<EmojiPickerWrapperProps>`
+export const EmojiPickerWrapper = styled.div`
   em-emoji-picker {
-    --border-radius: ${({ borderRadius }) => borderRadius || 'unset'};
-    --category-icon-size: ${({ categoryIconSize }) => categoryIconSize};
-    --background-rgb: ${({ backgroundRgb }) => backgroundRgb || 'unset'};
-    --color-border-over: ${({ colorBorderOver }) => colorBorderOver || 'unset'};
-    --color-border: ${({ colorBorder }) => colorBorder || 'unset'};
     --font-family: 'Lab Grotesque', -apple-system, BlinkMacSystemFont, Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans';
-    --font-size: ${({ fontSize }) => fontSize};
-    --rgb-accent: ${({ rgbAccent }) => rgbAccent || 'unset'};
-    --rgb-background: ${({ rgbBackground }) => rgbBackground || 'unset'};
-    --rgb-color: ${({ rgbColor }) => rgbColor || 'unset'};
-    --rgb-input: ${({ rgbInput }) => rgbInput || 'unset'};
-    --shadow: ${({ shadow }) => shadow || 'unset'};
+    --shadow: 'unset';
   }
 `;
