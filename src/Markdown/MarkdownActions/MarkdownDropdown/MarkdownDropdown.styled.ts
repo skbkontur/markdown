271697295848559a2fd199e4856d46b1dd4a0e22
Alexey Belkov <belkov@skbkontur.ru>
2026-03-19 16:56:07 +0500

    Fix MarkdownDropdown vertical align

--- src/Markdown/MarkdownActions/MarkdownDropdown/MarkdownDropdown.styled.ts
+++ src/Markdown/MarkdownActions/MarkdownDropdown/MarkdownDropdown.styled.ts
@@ -2,8 +2,7 @@ import styled from '../../../styles/styled-components';
 
 export const Wrapper = styled.div`
   display: flex;
-  align-items: end;
-  padding-bottom: 1px;
+  align-items: center;
 
   button {
     font-size: ${p => p.theme.elementsFontSize};
